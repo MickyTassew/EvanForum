1dbb8074ffa1810d423d592173b530ddb75bead0
MikiyasT22 <120289002+MickyTassew@users.noreply.github.com>
2024-06-27 09:47:48 -0500

    Update axiosConfig.js
    changed the base url

--- Eva-forum/client/evangadi-frontend/src/axiosConfig.js
+++ Eva-forum/client/evangadi-frontend/src/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const axiosBase = axios.create({
-    baseURL: 'http://localhost:3306/api'
-    // baseURL: 'sql3.freesqldatabase.com/api'
+    // baseURL: 'http://localhost:3306/api'
+    baseURL: 'https://evanforum-1.onrender.com/api'
 })
 
 export default axiosBase
